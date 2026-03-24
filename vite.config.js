6f4c7cec47e6b20a2d5128449c0d5fae5514ea4c
Bhye Muzaffar Alii Rohomuthally <rohomutm@myumanitoba.ca>
2026-03-24 16:38:02 +0400

    build with vite update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import laravel from "laravel-vite-plugin";
 import path from "path";
 
 export default defineConfig({
+    base: "/dev/",
     server: {
         host: "0.0.0.0",
         port: 5173,
