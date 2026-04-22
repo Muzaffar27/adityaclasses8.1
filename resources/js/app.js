2c10731b32030bc7031b8c28ba05ba855f711712
Bhye Muzaffar Alii Rohomuthally <rohomutm@myumanitoba.ca>
2026-04-22 19:35:41 +0400

    Reverted

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,7 @@ import App from "./components/App.vue";
 import "../sass/app.scss";
 import { useAuthStore } from "./stores/auth";
 
-document.documentElement.classList.add("theme-dark");
+// document.documentElement.classList.add("theme-dark");
 
 const app = createApp(App);
 const pinia = createPinia();
