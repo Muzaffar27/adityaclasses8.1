92e6fcb097dd2c80d3e109e4dc316458ed64ffed
Bhye Muzaffar Alii Rohomuthally <rohomutm@myumanitoba.ca>
2026-05-28 10:48:03 +0400

    PWA #2

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "aditya-classes-v4";
+const CACHE_VERSION = "aditya-classes-v5";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
@@ -7,11 +7,12 @@ const APP_SHELL_URLS = [
     "/manifest.webmanifest",
     "/logo.png",
     "/menu_logo.png",
+    "/pwa-icons/favicon-32.png",
     "/pwa-icons/icon-192.png",
     "/pwa-icons/icon-512.png",
     "/pwa-icons/maskable-512.png",
+    "/pwa-icons/apple-touch-icon.png",
     "/pwa-screenshots/wide.png",
-    "/pwa-screenshots/mobile.png",
     "/favicon.ico",
 ];
 
